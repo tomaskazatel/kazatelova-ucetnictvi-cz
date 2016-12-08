95ee050383effb0be5a61b059f62e4cf189d2349
Tomáš Kazatel <kazatel@jiktogroup.cz>
2016-12-08 16:41:12 +0100

    Edite gulp for TravisCI

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,52 @@ var webserver   = require('gulp-webserver');
 var opn         = require('opn');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
+var del         = require('del');
 
-gulp.task('sass', function(){
-    gulp.src('./source/css/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./source/css'))
+
+gulp.task('build', function () {
+
+    del(['./output/**/*']);
+
+   //copy html files to output
+   gulp.src('./source/**/*.html', {base: 'source'})
+       .pipe(gulp.dest('./output'));
+
+   //copy images
+   gulp.src('./source/**/*.{ico,png,jpeg,jpg,gif}', {base: 'source'})
+       .pipe(gulp.dest('./output'));
+
+   //generated css from sass
+   gulp.src('./source/css/*.scss')
+       .pipe(sass({outputStyle: 'compressed'}))
+       .pipe(gulp.dest('./output/css'));
+
+   //concat and minify js
+   gulp.src('./source/js/*.js')
+       .pipe(concat('js.min.js'))
+       .pipe(uglify())
+       .pipe(gulp.dest('./output/js'));
+
 });
 
-gulp.task('scripts', function () {
-    gulp.src(['./source/js/js.js'])
-        .pipe(concat('js.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./source/js'))
-});
+gulp.task('run', function () {
 
-gulp.task('webserver', function () {
-    gulp.src('./source')
-       .pipe(webserver({
-           host:                'localhost',
-           port:                '8000',
-           livereload:          true,
-           directoryListing:    false,
-       }));
-});
+    gulp.src('./output')
+        .pipe(webserver({
+            host:                'localhost',
+            port:                '8000',
+            livereload:          true,
+            directoryListing:    false
+        }));
 
-gulp.task('openbrowser', function () {
     opn('http://localhost:8000');
+
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./source/css/*.scss', ['sass']);
-    gulp.watch('./source/js/*.js', ['scripts']);
+
+    gulp.watch('./source/**/*', ['build']);
+
 });
 
-gulp.task('build', ['sass', 'scripts']);
-
-gulp.task('default', ['build', 'webserver', 'watch', 'openbrowser']);
+gulp.task('default', ['build','watch','run']);
